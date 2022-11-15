23194d837f5d7003a806c0fc892dffd4dd1ba5b9
vincent.oh <lawsnland@gmail.com>
2022-11-16 01:09:47 +0900

    Initial commit

---
+++ src/main/java/kr/doo/toy06redispostgre/Toy06RedispostgreApplication.java
@@ -0,0 +1,13 @@
+package kr.doo.toy06redispostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy06RedispostgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy06RedispostgreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/doo/toy06redispostgre/Toy06RedispostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.doo.toy06redispostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy06RedispostgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
